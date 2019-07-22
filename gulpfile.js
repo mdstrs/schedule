7e41690d33431245cc7866d8ca2a87de2f5abb65
mdstrs <hawaiian1820@gmail.com>
2019-07-22 15:29:49 +0500

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
